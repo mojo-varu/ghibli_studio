26fd7d1a629ffb71789a46905d2d68489f6b8988
Varunendra Singh <varunendra@seynse.com>
2019-12-03 04:30:41 +0300

    per-view cache implementation using memcache

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 
+from django.core.cache import cache
 from movies.dataservice import get_people_per_movie, get_films_people_mapping
 
 
@@ -14,7 +15,14 @@ class MoviesPeopleMapping(APIView):
 
 class MoviesView(APIView):
     def get(self, request, ):
-        people_in_movie = get_people_per_movie()
-        return Response(people_in_movie,
-                        status=status.HTTP_200_OK)
 
+        cache_key = 'ghibli_studio_cache'
+        cache_time = 60
+        result = cache.get(cache_key)
+
+        if not result:
+            result = get_people_per_movie()
+            cache.set(cache_key, result, cache_time)
+
+        return Response(result,
+                        status=status.HTTP_200_OK)
