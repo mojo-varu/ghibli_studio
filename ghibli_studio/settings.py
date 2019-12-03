fedbecfcae00f9928be47e661f8eac8d91a7824d
Varunendra Singh <varunendra@seynse.com>
2019-12-03 17:37:23 +0300

    resolved memcache connection issue

--- ghibli_studio/settings.py
+++ ghibli_studio/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'memcached:11211',
     }
 }
 
