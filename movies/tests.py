d51094197cf871d05f0a075611d1ff431bc817d0
Varunendra Singh <varunendra@seynse.com>
2019-12-04 13:54:18 +0300

    added test cases for urls and views

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from movies.views import MoviesView, MoviesPeopleMapping
 
-# Create your tests here.
+
+class TestUrls(TestCase):
+    """ Tests that URLs in movies app resolves to correct views """
+
+    def test_movies_list_url(self):
+        url = reverse('movies-list')
+        self.assertEqual(resolve(url).func.view_class, MoviesView)
+
+    def test_movies_people_mapping_url(self):
+        url = reverse('movies-people-mapping')
+        self.assertEqual(resolve(url).func.view_class, MoviesPeopleMapping)
+
+
+class TestViews(TestCase):
+    """ Tests that the response of the views are valid  """
+
+    def setUp(self):
+        self.client = Client()
+        self.movies_view_url = reverse('movies-list')
+        self.people_mapping_url = reverse('movies-people-mapping')
+
+        self.movies_view_template = 'movies/movies-list.html'
+
+    def test_movies_view_GET(self):
+        response = self.client.get(self.movies_view_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, self.movies_view_template)
+        self.assertTrue(isinstance(response.context['movies'], list))
+        self.assertNotEqual(len(response.context['movies']), 0)
+
+    def test_people_mapping_view_GET(self):
+        response = self.client.get(self.people_mapping_url)
+        self.assertEqual(response.status_code, 200)
+
+
