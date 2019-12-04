3c1c528d9b656485438e013feb41cf83ab5ae62b
Varunendra Singh <varunendra@seynse.com>
2019-12-04 11:19:56 +0300

    structured the API calls into a class

--- movies/dataservice.py
+++ movies/dataservice.py
@@ -1,43 +1,69 @@
 from django.conf import settings
 import json
 import requests
-import datetime
 
 
-def get_all_films():
-    response = requests.get(settings.STUDIO_GHIBLI_BASE_URL+'/films/',
-                            params={'limit': '250'})
-    print("Films Response Status : ", response.status_code)
-    films = json.loads(response.text)
-    return films
+class GhibliStudio:
 
+    def __init__(self):
+        self._api_base_url = settings.STUDIO_GHIBLI_BASE_URL
+        self._limit_params = {'limit': '250'}
 
-def get_films_people_mapping():
-    response = requests.get(settings.STUDIO_GHIBLI_BASE_URL+'/people/',
-                            params={'limit': '250'})
-    people = json.loads(response.text)
-    movie_people_mapping = {}
-    for person in people:
-        for film_url in person['films']:
-            film_id = film_url.split('/')[-1]
-            if film_id not in movie_people_mapping.keys():
-                movie_people_mapping[film_id] = [person['name']]
-            else:
-                movie_people_mapping.get(film_id).append(person['name'])
+    def _get_movies(self):
+        response = requests.get(self._api_base_url+'/films/',
+                                params=self._limit_params)
+        if response.status_code != requests.codes.ok:
+            return
 
-    return movie_people_mapping
+        movies = json.loads(response.text)
+        return movies
 
+    def _get_people(self):
+        response = requests.get(self._api_base_url + '/people/',
+                                params=self._limit_params)
+        if response.status_code != requests.codes.ok:
+            return
 
-def get_people_per_movie():
-    films = get_all_films()
-    movie_people_mapping = get_films_people_mapping()
-    people_per_movie = []
-    for film in films:
-        people_per_movie.append({'movie_name': film['title'],
-                                 'movie_id': film['id'],
-                                 'people_in_movie':
-                                     movie_people_mapping.get(film['id']),
-                                 'updated_at':
-                                     datetime.datetime.now().strftime(
-                                         "%m/%d/%Y, %H:%M:%S")})
-    return people_per_movie
+        people = json.loads(response.text)
+        return people
+
+    def get_movies_people_mapping(self):
+        people = self._get_people()
+        if not people:
+            return
+
+        movies_people_mapping = {}
+        for person in people:
+            person_name = person.get('name')
+            movies_performed = person.get('films')
+
+            if not movies_performed:
+                continue
+
+            for movie_url in movies_performed:
+                movie_id = movie_url.split('/')[-1]
+                if movie_id not in movies_people_mapping:
+                    movies_people_mapping[movie_id] = [person_name]
+                else:
+                    movies_people_mapping.get(movie_id).append(person_name)
+
+        return movies_people_mapping
+
+    def get_people_per_movie(self):
+        movies = self._get_movies()
+        if not movies:
+            return
+
+        movies_with_people = self.get_movies_people_mapping()
+        if not movies_with_people:
+            return
+
+        people_per_movie = []
+        for movie in movies:
+            people_per_movie.append({'movie_name': movie.get('title'),
+                                     'people_performed':
+                                         movies_with_people.get(
+                                             movie.get('id'))})
+
+        return people_per_movie
+
