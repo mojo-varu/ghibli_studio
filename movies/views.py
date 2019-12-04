7c725d6318b0762cc4ab9e497da56949b7d77c22
Varunendra Singh <varunendra@seynse.com>
2019-12-04 11:23:06 +0300

    updated the views

--- movies/views.py
+++ movies/views.py
@@ -8,30 +8,32 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from movies.dataservice import get_people_per_movie, get_films_people_mapping
+from movies.dataservice import GhibliStudio
 
 
 class MoviesPeopleMapping(APIView):
     def get(self, request, *args, **kwargs):
-        films_people = get_films_people_mapping()
-        if films_people:
-            return Response(films_people, status=status.HTTP_200_OK)
-        else:
+        ghibli_studio = GhibliStudio()
+        movies_people = ghibli_studio.get_movies_people_mapping()
+        if not movies_people:
             return Response('People are unpredictable, they go missing !',
                             status=status.HTTP_404_NOT_FOUND)
+
+        return Response(movies_people, status=status.HTTP_200_OK)
 
 
 class MoviesView(APIView):
     def get(self, request, *args, **kwargs):
         result = cache.get(settings.CACHE_KEY)
         if not result:
-            result = get_people_per_movie()
+            ghibli_studio = GhibliStudio()
+            result = ghibli_studio.get_people_per_movie()
             cache.set(settings.CACHE_KEY, result,
                       settings.CACHE_TIMEOUT_IN_SECS)
-        if result:
-            return render(request, 'movies/movies-list.html',
-                          {'movies': result})
-        else:
+        if not result:
             return HttpResponseNotFound('<h2>Something wrong with movies !<br>'
                                         'Meanwhile read books until '
                                         'we fix this.</h2>')
+
+        return render(request, 'movies/movies-list.html',
+                      {'movies': result})
